8d6be69be89230a6bc0bbb29c5709c51b6b1e7bd
nomvula <nzshabangu95@gmail.com>
2023-09-14 13:54:23 +0200

    Add project files.

---
+++ Getters and Setters 2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Getters_and_Setters_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student("jeff", 23, "mtt");
+            Console.WriteLine(student1.gender);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Getters and Setters 2/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Getters_and_Setters_2
+{
+    class Student
+    {
+        public string name;
+        public int age;
+        public string gender;
+
+        //contructor
+        public  Student( string aName, int aAge, string aGender)
+        {
+            name = aName;
+            age = aAge;
+            Gender = aGender;
+        }
+
+        public string Gender
+        {
+            get
+            {
+                return  gender;
+            }
+            set
+            {
+                if(value == "male" || value == "female")
+                {
+                    gender = value;
+                }
+                else
+                {
+                    gender = "NOT GENDER";
+                }
+            }
+        }
+    }
+}
